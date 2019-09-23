b3de434e2f0eb5ccab7b098ca8dbec7a32d6bde3
Joseph Adediji <josylado@gmail.com>
2019-09-23 17:10:47 +0300

    heroku deployment

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
     
     #  email configurations
